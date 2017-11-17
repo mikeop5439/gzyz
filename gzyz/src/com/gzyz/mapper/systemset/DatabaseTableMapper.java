7a8ef370292bfd00fad55e9e512e4240829f2ba9
mikeop5439 <704824975@qq.com>
2017-11-17 17:27:13 +0800

    remove directory from remote repository

--- gzyz/src/com/gzyz/mapper/systemset/DatabaseTableMapper.java
+++
@@ -1,7 +0,0 @@
-package com.gzyz.mapper.systemset;
-
-import com.gzyz.bean.systemset.DatabaseTable;
-
-public interface DatabaseTableMapper {
-	public DatabaseTable findDatabaseTable(DatabaseTable databaseTable);
-}
--- gzyz/src/com/gzyz/service/reception/UserLoginService.java
+++
@@ -1,8 +0,0 @@
-package com.gzyz.service.reception;
-
-import com.gzyz.bean.reception.InsertInfo;
-
-public interface UserLoginService {
-	public int countUsername(String user_name);
-	public void insertUsername(InsertInfo insertInfo);
-}
