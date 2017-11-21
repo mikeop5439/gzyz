a60252070d0bc6208a37ec692f31fc1fde79e99e
KCI7 <673402807@qq.com>
2017-11-21 09:53:17 +0800

    商品详情页1.0

---
+++ gzyz/src/com/gzyz/service/introduction/serviceImpl/IntroductionServiceImpl.java
@@ -0,0 +1,10 @@
+package com.gzyz.service.introduction.serviceImpl;
+
+import org.springframework.stereotype.Service;
+
+import com.gzyz.service.introduction.service.IntroductionService;
+
+@Service
+public class IntroductionServiceImpl implements IntroductionService {
+
+}
