add2e97eb8477aa91480559dc2e8f329e86b056f
KCI7 <673402807@qq.com>
2017-11-28 11:11:38 +0800

    添加了商品详情页推荐商品的链接

--- gzyz/src/com/gzyz/bean/introduction/extend/RelatedGoodsKey.java
+++ gzyz/src/com/gzyz/bean/introduction/extend/RelatedGoodsKey.java
@@ -1,9 +1,18 @@
 package com.gzyz.bean.introduction.extend;
 
 public class RelatedGoodsKey {
+	private int goods_id;
 	private String keywords;
 	private int firstnum;
 	private int secondnum;
+	
+	
+	public int getGoods_id() {
+		return goods_id;
+	}
+	public void setGoods_id(int goods_id) {
+		this.goods_id = goods_id;
+	}
 	public String getKeywords() {
 		return keywords;
 	}
@@ -24,8 +33,9 @@ public class RelatedGoodsKey {
 	}
 	@Override
 	public String toString() {
-		return "RelatedGoodsKey [keywords=" + keywords + ", firstnum="
-				+ firstnum + ", secondnum=" + secondnum + "]";
+		return "RelatedGoodsKey [goods_id=" + goods_id + ", keywords="
+				+ keywords + ", firstnum=" + firstnum + ", secondnum="
+				+ secondnum + "]";
 	}
 	
 }
